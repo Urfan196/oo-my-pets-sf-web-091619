cfb1f80744e98822deecb5ea5df49fb7404fd81a
Urfan Guliyev <urfan196@yahoo.com>
2019-09-23 03:16:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,17 +1,13 @@
 class Owner
 
-attr_reader :name
+attr_reader :name, :species
 
 @@all = []
 
   def initialize (name)
   @name = name
-  @species = human
+  @species = 'human'
   @@all << self
-  end
-
-  def say_species
-    @species
   end
 
   def self.all
@@ -25,5 +21,35 @@ attr_reader :name
   def self.reset_all
     @@all.clear
   end
+
+  def cats
+    Cat.all.select do {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select do {|dog| dog.owner == self}
+  end
+
+  def buy_cat (name)
+    Cat.new(name,self)
+  end
+
+  def buy_dog (name)
+    Dog.new(name,self)
+  end
+
+  def walk_dogs
+    Dog.all.each do |dog|
+      dog.mood == "happy"
+    end
+  end
+
+  def feed_cats
+    Cat.all.each do |cat|
+      cat.mood == "happy"
+    end
+  end
+
+  
 
 end
